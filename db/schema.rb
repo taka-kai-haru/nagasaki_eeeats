e1263eb0d99f8cabc514956c193c6f0201450fab
taka-kai-haru <erie.orchid.dev@gmail.com>
2020-08-24 20:20:52 +0900

    address radio button changed

--- db/schema.rb
+++ db/schema.rb
@@ -41,22 +41,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_195022) do
     t.index ["prefecture_id"], name: "index_areas_on_prefecture_id"
   end
 
-  create_table "evaluation_types", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "evaluations", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "evaluation_type_id", null: false
-    t.integer "score"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["evaluation_type_id"], name: "index_evaluations_on_evaluation_type_id"
-    t.index ["post_id"], name: "index_evaluations_on_post_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.integer "restaurant_id", null: false
     t.integer "user_id", null: false
@@ -125,8 +109,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_195022) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "areas", "prefectures"
-  add_foreign_key "evaluations", "evaluation_types"
-  add_foreign_key "evaluations", "posts"
   add_foreign_key "posts", "restaurants"
   add_foreign_key "posts", "users"
   add_foreign_key "restaurants", "areas"
