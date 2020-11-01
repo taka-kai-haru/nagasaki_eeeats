594061cf01dece05836ff49e615f34b82a3a758e
taka-kai-haru <erie.orchid.dev@gmainl.com>
2020-11-01 10:57:13 +0900

    recommit

--- app/javascript/packs/home/restaurants/show_map.js
+++ app/javascript/packs/home/restaurants/show_map.js
@@ -10,6 +10,13 @@ let map;
 let gps_marker = null;
 let directionsService = new google.maps.DirectionsService();
 let directionsRenderer = new google.maps.DirectionsRenderer({suppressMarkers: true}); //suppressMarkers: true // デフォルトのマーカーを削除
+let directionsServiceWork = new google.maps.DirectionsService();
+let directionsRendererWork = new google.maps.DirectionsRenderer({suppressMarkers: true,
+      polylineOptions: {
+      strokeColor: '#98fb98',
+      strokeOpacity: 0.5,
+      strokeWeight: 3}
+}); //suppressMarkers: true // デフォルトのマーカーを削除
 let marker = null;
 
 
@@ -78,6 +85,7 @@ gps_fixed.addEventListener('click', (event) => {
     //     marker.setMap(null);
     // ルート描画
     getRoute(latlng);
+    getRouteWork(latlng);
     // 距離描画
     getDestance(lat,lng);
   },
@@ -92,11 +100,24 @@ function getRoute(latlng){
   let request = {
     origin: latlng, //入力地点の緯度、経度
     destination: point, //到着地点の緯度、経度
-    travelMode: google.maps.DirectionsTravelMode.WALKING //ルートの種類
+    travelMode: google.maps.DirectionsTravelMode.DRIVING //ルートの種類
   }
   directionsService.route(request,function(result, status){
     directionsRenderer.setDirections(result); //取得した情報をset
     directionsRenderer.setMap(map); //マップに描画
+  });
+}
+
+//ルート描画用
+function getRouteWork(latlng){
+  let request = {
+    origin: latlng, //入力地点の緯度、経度
+    destination: point, //到着地点の緯度、経度
+    travelMode: google.maps.DirectionsTravelMode.WALKING //ルートの種類
+  }
+  directionsServiceWork.route(request,function(result, status){
+    directionsRendererWork.setDirections(result); //取得した情報をset
+    directionsRendererWork.setMap(map); //マップに描画
   });
 }
 
@@ -148,4 +169,51 @@ function getDestance(gpslat,gpslng){
   });
 }
 
+// 距離取得
+function getDestanceWork(gpslat,gpslng){
+
+  // DistanceMatrix サービスを生成
+  let distanceMatrixService = new google.maps.DistanceMatrixService();
+
+  // 出発点
+  let origns = [new google.maps.LatLng(gpslat, gpslng)];
+  // 到着点
+  let destinations = [new google.maps.LatLng(latitude, longitude)];
+
+  // DistanceMatrix の実行
+  distanceMatrixService.getDistanceMatrix({
+    origins: origns, // 出発地点
+    destinations: destinations, // 到着地点
+    travelMode: google.maps.TravelMode.WALKING, // 車モード or 徒歩モード
+    unitSystem: google.maps.UnitSystem.METRIC,
+    avoidHighways: false,
+    avoidTolls: false
+    // drivingOptions: { // 車モードの時のみ有効
+    //   departureTime: new Date('2017/5/5 10:00:00'), // 2017年5月5日
+    //   trafficModel: google.maps.TrafficModel.BEST_GUESS // 最適な検索
+    // }
+  }, function(response, status) {
+    if (status == google.maps.DistanceMatrixStatus.OK) {
+
+      // 出発地点と到着地点の住所（配列）を取得
+      var origins = response.originAddresses;
+      var destinations = response.destinationAddresses;
+
+      // 出発地点でループ
+      for (var i=0; i<origins.length; i++) {
+        // 出発地点から到着地点への計算結果を取得
+        var results = response.rows[i].elements;
+
+        // 到着地点でループ
+        for (var j = 0; j<results.length; j++) {
+          // var from = origins[i]; // 出発地点の住所
+          // var to = destinations[j]; // 到着地点の住所
+          document.getElementById("duration-work").innerText = results[j].duration.text; // 時間
+          document.getElementById("distance-work").innerText = results[j].distance.text; // 距離
+          // console.log("{},{},{},{}", from,  to, duration, distance);
+        }
+      }
+    }
+  });
+}
 
