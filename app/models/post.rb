d4eafa99f252c0071dc09504166da09587feb87e
taka-kai-haru <erie.orchid.dev@gmainl.com>
2020-11-13 05:59:06 +0900

    actiontext validate add

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,14 @@ class Post < ApplicationRecord
                                                 message: "jpeg,gif,pngの拡張子にしてください。" },
                                                 size:         { less_than: 5.megabytes,
                                                 message: "5MBより小さくしてください。" }
+  validate :validate_comment_attachment_byte_size
+  validate :validate_comment_attachments_count
+
   IMAGE_MAX_SAVE = 3
+  ONE_KILOBYTE = 1024
+  MEGA_BYTES = 3
+  MAX_COMMENT_ATTACHMENT_BYTE_SIZE = MEGA_BYTES * 1000 * ONE_KILOBYTE
+  MAX_COMMENT_ATTACHMENTS_COUNT = 4
 
 
   # Restaurant Point更新
@@ -30,6 +37,32 @@ class Post < ApplicationRecord
     restaurant = Restaurant.find_by(id: restaurant_id)
     restaurant.point = (point / post_count).round(1)
     restaurant.save
+  end
+
+  private
+
+  def validate_comment_attachment_byte_size
+    comment.body.attachables.grep(ActiveStorage::Blob).each do |attachabl|
+      if attachabl.byte_size > MAX_COMMENT_ATTACHMENT_BYTE_SIZE
+        errors.add(
+            :base,
+            :comment_attachement_byte_size_is_too_big,
+            max_comment_attachement_mega_byte_size: MEGA_BYTES,
+            bytes: attachabl.byte_size,
+            max_bytes: MAX_COMMENT_ATTACHMENT_BYTE_SIZE
+        )
+      end
+    end
+  end
+
+  def validate_comment_attachments_count
+    if comment.body.attachables.grep(ActiveStorage::Blob).count > MAX_COMMENT_ATTACHMENTS_COUNT
+      errors.add(
+          :content,
+          :attachments_count_is_too_big,
+          max_comment_attachments_count: MAX_COMMENT_ATTACHMENTS_COUNT
+      )
+    end
   end
 
 end
